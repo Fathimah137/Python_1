6bd288bb1271bf7a6cb72fb4c9ab96a6ac7187e5
Fathimah137 <fathimahshibin@gmail.com>
2026-05-14 17:13:07 +0400

    Add files via upload

---
+++ type.py
@@ -0,0 +1,19 @@
+#integer
+z=7
+print(z)
+print(type(z), "\n")
+
+#float
+s=3.1
+print(s)
+print(type(s), "\n")
+
+#string
+j="hello"
+print(j)
+print(type(j), "\n")
+
+#boolean
+h=True
+print(h)
+print(type(h), "\n")
